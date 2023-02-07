c5cd1c1e555e1f7eea89e8111b6e14c77b90bbd5
sawmon <sawmonhan@acedatasystems.com>
2023-02-07 05:50:08 +0100

    fixed linters errors

--- script.js
+++ script.js
@@ -1,24 +1,44 @@
-const ham = document.querySelector(".hamburger");
-const menulinks = document.querySelector(".nav-links");
+const brand = document.querySelector('.brand');
+const checkbox = document.querySelector('.toggler');
+const intro = document.querySelector('.bio-section');
+const hamburger = document.querySelector('.hamburger');
+const works = document.querySelector('.works');
+const aboutMyself = document.querySelector('.about-section');
 
-ham.addEventListener("click", () => {
-    ham.classList.toggle("active");
-    menulinks.classList.toggle("active");
-})
+checkbox.addEventListener('click', () => {
+  brand.classList.toggle('blur');
+  intro.classList.toggle('blur');
+  works.classList.toggle('blur');
+  aboutMyself.classList.toggle('blur');
 
+  navlinks.addEventListener('click', () => {
+    document.getElementsByClassName('toggler').checked = false;
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
+    hamburger.style.position = 'fixed';
+  });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-ham.classList.remove("active");
-  menulinks.classList.remove("active");
-}))
+  if (checkbox.checked) {
+    hamburger.style.position = 'fixed';
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
+  } else {
+    hamburger.style.position = 'absolute';
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
+let w = 0;
+window.onresize = () => {
+  w = window.innerWidth;
+  if (w > 768) {
+    document.getElementsByClassName('toggler').checked = false;
+    brand.classList.remove('blur');
+    intro.classList.remove('blur');
+    works.classList.remove('blur');
+    aboutMyself.classList.remove('blur');
+  }
+};
