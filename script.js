fef6a8156ae27ae18d68649c22887c8507cefdba
sawmon <sawmonhan@acedatasystems.com>
2023-02-09 11:12:12 +0100

    work section fixed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const brand = document.querySelector('.brand');
-const checkbox = document.querySelector('.toggler');
-const intro = document.querySelector('.intro-center');
+const checkbox = document.querySelector('#toggle');
+const intro = document.querySelector('.intro');
 const hamburger = document.querySelector('.hamburger');
 const works = document.querySelector('.works');
 const aboutMyself = document.querySelector('.about_myself');
-const form = document.querySelector('.contact_form');
 const navLinks = document.querySelectorAll('.nav-links');
 
 function remove() {
@@ -13,18 +12,16 @@ function remove() {
   intro.classList.remove('blur');
   works.classList.remove('blur');
   aboutMyself.classList.remove('blur');
-  form.classList.remove('blur');
   hamburger.style.position = 'absolute';
 }
 
 function toggle() {
   if (checkbox.checked) {
     hamburger.style.position = 'fixed';
-    brand.classList.add('blur');
-    intro.classList.add('blur');
-    works.classList.add('blur');
-    aboutMyself.classList.add('blur');
-    form.classList.add('blur');
+    brand.classList.toggle('blur');
+    intro.classList.toggle('blur');
+    works.classList.toggle('blur');
+    aboutMyself.classList.toggle('blur');
   } else {
     hamburger.style.position = 'absolute';
     remove();
@@ -39,108 +36,42 @@ window.onresize = () => {
   }
 };
 
-navLinks.forEach((link) => link.addEventListener('click', remove));
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// eventlistener for open click
-btn.addEventListener("click", openModal);
-
-//eventlistener for close
-span.addEventListener("click", closeModal);
-//eventlistener for window
-window.addEventListener("click", outsideClick);
-// When the user clicks the button, open the modal 
-  function openModal() {
-   modal.style.display = "block";
- }
-
- //When the user clicks on <span> (x), close the modal
-  function closeModal() {
-   modal.style.display = "none";
- }
-
-// When the user clicks anywhere outside of the modal, close it
-function outsideClick(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+// Model
+const overlayModal = document.getElementById('overlayModal');
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  window.scrollTo(0, 0);
+  overlayModal.classList.add('active');
+  navLinks.forEach((link) => {
+    link.style.color = '#c1c7d0';
+  });
 }
 
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  modal.innerHTML = '';
 
-window.onload = () => {
-  const cards = [
-  {
-    
-      title: 'Library',
-      information:
-        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit.',
-      image: 'portfolio_1.png',
-      technologies: ['html', 'css', 'javascript'],
-      live: "https://david30-maker.github.io/",
-      source: "https://github.com/david30-maker/david30-maker.github.io/application-deploy/",
-    },
+  overlayModal.classList.remove('active');
+  navLinks.forEach((link) => {
+    if (w > 768) {
+      link.style.color = '#344563';
+    } else {
+      link.style.color = '#fff';
+    }
+  });
+}
 
-    {
-    
-      title: 'Library',
-      information:
-        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit.',
-      image: 'portfolio_2.png',
-      technologies: ['html', 'css', 'javascript'],
-      live: "https://david30-maker.github.io/",
-      source: "https://github.com/david30-maker/david30-maker.github.io/application-deploy/",
-    },
-
-    {
-    
-      title: 'Library',
-      information:
-        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit.',
-      image: 'portfolio_3.png',
-      technologies: ['html', 'css', 'javascript'],
-      live: "https://david30-maker.github.io/",
-      source: "https://github.com/david30-maker/david30-maker.github.io/application-deploy/",
-    },
-
-    {
-    
-      title: 'Library',
-      information:
-        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit.',
-      image: 'portfolio_4.png',
-      technologies: ['html', 'css', 'javascript'],
-      live: "https://david30-maker.github.io/",
-      source: "https://github.com/david30-maker/david30-maker.github.io/application-deploy/",
-    },
-
-
-]
-
-// cards.forEach(function (card, index, arr) {
-//   console.log(`Index: ${index}, Name: ${card.title}`);
-// });
-
-const projectSection = document.querySelector(".works")
-cards.forEach((card) => {
-  cardHtml = `
-      <div class="card" class="modal-content">
-        <header>
-          <img class="portfolio" src="./images/${card.image}" alt="Portfolio">
-        </header>
-        <div class="card_body">
-          <span class="close">&times;</span>
-          <h2 class="title">
-            ${card.title}
-          </h2>
+function createCard(project, index) {
+  const dataStr = encodeURIComponent(JSON.stringify(project));
+  const projectTemplate = `<div class="card">
+        <header id="header_${(index += 1)}" >
+          <img class="portfolio" src="./img/${project.featureImage}" 
+          alt="Portfolio">
+         </header>
+         <div class="card_body cb_${index}">
+          <h2 class="title">${project.name}</h2>
           <div class="experience">
           <span class="subtitle">Canopy</span>
           <ul class="block">
@@ -148,24 +79,183 @@ cards.forEach((card) => {
             <li>2015</li>
           </ul>
           </div>
-          </div>
-          <p class="content">  
-            ${card.information}
-          </p>
-          <ul class="skills card_${index}">
-           
+          <p class="content">${project.description}
+          </p>              
+          <ul class="skills project_${index}">
           </ul>
-          <div class="modal-buttons">
-            <a href="${card.live}"><button class="button"><p class="bmr-14">See Live</p> <img  src="img/Icon.png" alt="live icon"></button></a>
-            <a href="${card.source}"><button class="button"><p class="bmr-14">See Source</p> <a class="icons" href="https://github.com/david30-maker">
-            <ion-icon name="logo-github"></ion-icon></button></a>
-          </div>
-        </div>
-      </div>`
+          <button class="button" data-project=${dataStr} data-toggle="modal" data-modal-target="#modal" > See project </button>
+         </div>
+        </div>`;
 
-  // console.log(projectSection)
-  // console.log(cardHtml)
-  projectSection.innerHTML += cardHtml
-})
+  works.innerHTML += projectTemplate;
+  project.techStack.forEach((item) => {
+    const list = document.createElement('li');
+    list.innerText = item;
+    document.querySelector(`.project_${index}`).appendChild(list);
+  });
 
+  if (index % 2 === 0) {
+    const header = document.querySelector(`#header_${index}`);
+    const cardBody = document.querySelector(`.cb_${index}`);
+    header.classList.toggle('flip1');
+    cardBody.classList.toggle('flip2');
+  }
 }
+
+navLinks.forEach((link) => link.addEventListener('click', remove));
+
+window.onload = () => {
+  const projects = [
+    {
+      name: 'Library',
+      description:
+        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit.',
+      featureImage: 'portfolio1.png',
+      techStack: ['html', 'css', 'javascript'],
+      liveLink: 'https://sawmon71293.github.io/personal-portfolio/',
+      sourceLink: 'https://github.com/sawmon71293/personal-portfolio',
+    },
+    {
+      name: 'Job Agency',
+      description:
+        'A daily selection of privately personalized readsr sit amet consectetur adipisicing elit.',
+      featureImage: 'portfolio2.png',
+      techStack: ['html', 'css', 'javascript'],
+      liveLink: 'https://sawmon71293.github.io/personal-portfolio/',
+      sourceLink: 'https://github.com/sawmon71293/personal-portfolio',
+    },
+    {
+      name: 'Songs Library',
+      description:
+        'A daily selection of privately personalized reads dolor sit amet consectetur adipisicing elit. ',
+      featureImage: 'portfolio3.png',
+      techStack: ['html', 'react', 'Ruby on Rails'],
+      liveLink: 'https://sawmon71293.github.io/personal-portfolio/',
+      sourceLink: 'https://github.com/sawmon71293/personal-portfolio',
+    },
+    {
+      name: 'Portal',
+      description:
+        'A daily selection of privately personalized reads sit amet consectetur adipisicing elit. ',
+      featureImage: 'portfolio4.png',
+      techStack: ['html', 'react', 'Ruby on Rails'],
+      liveLink: 'https://sawmon71293.github.io/personal-portfolio/',
+      sourceLink: 'https://github.com/sawmon71293/personal-portfolio',
+    },
+  ];
+  projects.forEach((project, index) => {
+    createCard(project, index);
+  });
+
+  const openModalButtons = document.querySelectorAll('[data-modal-target]');
+
+  openModalButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget);
+      const project = JSON.parse(decodeURIComponent(button.dataset.project));
+
+      const projectModalTemplate = `
+         <header class="modal-header">
+          <div><h2 id="modal-title">${project.name}</h2></div>
+          <button data-close-button class="close-button">&times;</button>
+         </header>
+          <div class="wrap">
+         <div class="modal-body">
+          <ul class="modal-exp">
+            <li class="canopy">CANOPY</li>
+            <li>Back End Dev</li>
+            <li>2015</li>
+          </ul>
+          <img class="portfolio" src="./img/${project.featureImage}" alt="Portfolio">
+          <div class="project-wrapper">
+            <div class="description">
+              <p>${project.description}
+              </p>
+            </div>
+            <div>
+              <ul class="modal-skills" id="modal-skills">
+              </ul>
+              <div class="modal-buttons">
+                <a href="${project.liveLink}"><button class="button mr12"><p class="bmr-14">See Live</p> <img  src="img/Icon.png" alt="live icon"></button></a>
+                <a href="${project.sourceLink}"><button class="button"><p class="bmr-14">See Source</p> <ion-icon class="github"  name="logo-github"></ion-icon></button></a>
+              </div>
+            </div>
+          </div>
+         </div>
+       </div>`;
+
+      modal.innerHTML += projectModalTemplate;
+      project.techStack.forEach((item) => {
+        const listSkills = document.createElement('li');
+        listSkills.innerText = item;
+        document.querySelector('.modal-skills').appendChild(listSkills);
+      });
+      hamburger.style.display = 'none';
+      openModal(modal);
+      const closeModalButton = document.querySelector('[data-close-button]');
+      closeModalButton.addEventListener('click', () => {
+        const modal = closeModalButton.closest('.modal');
+        hamburger.style.display = 'flex';
+        closeModal(modal);
+      });
+    });
+  });
+
+  overlayModal.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach((modal) => {
+      closeModal(modal);
+    });
+  });
+};
+
+// form validation
+function validation() {
+  const email = document.getElementById('email').value;
+  const errorMessage = document.getElementById('error');
+  errorMessage.style.display = 'block';
+  if (email !== String(email).toLowerCase()) {
+    const text = 'Please enter a valid email!';
+    errorMessage.innerText = text;
+    errorMessage.style.background = 'red';
+    errorMessage.style.color = 'black';
+  } else {
+    errorMessage.innerText = 'The form is sent';
+    errorMessage.style.background = 'green';
+    errorMessage.style.color = 'white';
+  }
+}
+
+//  save objects to LocalStorage
+
+const form = document.querySelector('form');
+const formEls = ['input[type=text]', 'input[type=email]', 'textarea'];
+
+function getAllFormEls() {
+  const els = formEls.map((el) => Array.from(form.querySelectorAll(el)));
+  return els.flat();
+}
+
+function persistForm() {
+  const els = getAllFormEls();
+  els.forEach((el) => {
+    el.addEventListener('change', () => {
+      localStorage.setItem(el.name, el.value);
+    });
+  });
+}
+
+function restoreForm() {
+  const els = getAllFormEls();
+  els.forEach((el) => {
+    el.value = localStorage.getItem(el.name);
+  });
+}
+
+restoreForm();
+persistForm();
+
+const submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+  validation();
+});
