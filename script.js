9ea24af42aa05d1d3adac4589fdbdad762d4c879
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-02-10 10:39:48 +0100

    Add to changes to javascript file

--- script.js
+++ script.js
@@ -210,21 +210,7 @@ window.onload = () => {
 };
 
 // form validation
-function validation() {
-  const email = document.getElementById('email').value;
-  const errorMessage = document.getElementById('error');
-  errorMessage.style.display = 'block';
-  if (email !== String(email).toLowerCase()) {
-    const text = 'Please enter a valid email!';
-    errorMessage.innerText = text;
-    errorMessage.style.background = 'red';
-    errorMessage.style.color = 'black';
-  } else {
-    errorMessage.innerText = 'The form is sent';
-    errorMessage.style.background = 'green';
-    errorMessage.style.color = 'white';
-  }
-}
+
 
 //  save objects to LocalStorage
 
